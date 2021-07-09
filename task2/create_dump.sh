e14add8ec3b03367e8cd8d2f162f09c5635a7bd4
Ubuntu <ubuntu@ip-172-31-12-195.eu-central-1.compute.internal>
2021-07-09 10:26:35 +0000

    Add task2

---
+++ task2/create_dump.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Backup started"
+
+# The time of creating a dump
+current_date=$(date +"%Y_%m_%d_%H:%M:%S")
+
+# Create a directory where we will save the dump
+mkdir ./db_dumps
+
+cd /home/ubuntu/db_dumps
+
+# create a dump
+mysqldump -u moodleuser -p moodle > ~/db_dumps/moodle_$current_date
+
+echo "Backup finished"
